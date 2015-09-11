b5d6ca1eac57ddbea5fca7219f92e29e0f1419b7
Romain <romain.racamier@gmail.com>
2015-09-11 15:17:52 +0200

    added view table photos

---
+++ app/php/views/tablePhotosModal.php
@@ -0,0 +1,10 @@
+<div id="tablePhotosModal" tabindex="-1" role="dialog" class="modal fullscreen fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <?php require 'tablePhotos.php'; ?>
+            </div>
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+    </div>
+</div>
